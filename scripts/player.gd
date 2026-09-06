2906168013d70a897408d559e3ae20ee34d6a2a1
kyoumaru <166464753+kyoumaru@users.noreply.github.com>
2026-09-06 18:01:22 +0800

    fixing arrow tiles bug

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,20 +3,32 @@ const TILE_SIZE := 16
 const SPEED := 100.0
 var is_moving := false
 var move_direction := Vector2.ZERO
+var facing_direction := Vector2.ZERO
 var target_position := Vector2.ZERO
+var keep_playing_animation := false
 
+@onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var tilemap: TileMap = get_node("../TileMap")
 
 func _ready() -> void:
 	target_position = global_position
+	animated_sprite_2d.play("idle_r")
 
-func _physics_process(delta: float) -> void:
-	if is_moving:
-		move_to_target(delta)
-		return
-	var direction := get_input_direction()
-	if direction != Vector2.ZERO:
-		start_move(direction)
+func move_animation(direction: Vector2) -> void:
+	if direction == Vector2.UP:
+		animated_sprite_2d.play("walking_north")
+		facing_direction = Vector2.UP
+	elif direction == Vector2.DOWN:
+		animated_sprite_2d.play("walking_south")
+		facing_direction = Vector2.DOWN
+	elif direction == Vector2.LEFT:
+		animated_sprite_2d.flip_h = true
+		facing_direction = Vector2.RIGHT
+		animated_sprite_2d.play("walking")
+	elif direction == Vector2.RIGHT:
+		facing_direction = Vector2.RIGHT
+		animated_sprite_2d.flip_h = false
+		animated_sprite_2d.play("walking")
 
 func get_input_direction() -> Vector2:
 	if move_direction == Vector2.UP and Input.is_action_pressed("move_up"):
@@ -37,20 +49,35 @@ func get_input_direction() -> Vector2:
 		return Vector2.RIGHT
 	return Vector2.ZERO
 
+func _physics_process(delta: float) -> void:
+	move_to_target(delta)
+	if is_moving == false:
+		if facing_direction == Vector2.UP: animated_sprite_2d.play("idle_north")
+		elif facing_direction == Vector2.DOWN: animated_sprite_2d.play("idle_south")
+		else: animated_sprite_2d.play("idle_r")
+	var direction = get_input_direction()
+	start_move(direction)
+
 func start_move(direction: Vector2) -> void:
+	if is_moving:
+		return
+	if direction == Vector2.ZERO:
+		return
 	if test_move(global_transform, direction * TILE_SIZE):
 		return
 	move_direction = direction
 	target_position = global_position + direction * TILE_SIZE
 	is_moving = true
+	move_animation(direction)
 
 func move_to_target(delta: float) -> void:
 	var remaining := target_position - global_position
 	var step_distance := SPEED * delta
 	if remaining.length() <= step_distance:
 		global_position = target_position
-		is_moving = false
-		apply_tile_effects()
+		if is_moving:
+			is_moving = false
+			apply_tile_effects()
 		return
 	move_and_collide(move_direction * step_distance)
 
@@ -73,6 +100,7 @@ func force_move(direction: Vector2) -> void:
 	move_direction = direction
 	target_position = global_position + direction * TILE_SIZE
 	is_moving = true
+	move_animation(direction)
 
 func get_current_tile_data() -> TileData:
 	var cell := tilemap.local_to_map(tilemap.to_local(global_position))
