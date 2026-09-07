c7209fc871c2336708b1f8bc640c669ff4dd3987
Enrico Cruel <pio.cruel@student.ateneo.edu>
2026-09-07 22:27:20 +0800

    Added bonus tiles and updated collisions for existing tiles.

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,7 @@ var move_direction := Vector2.ZERO
 var facing_direction := Vector2.ZERO
 var target_position := Vector2.ZERO
 var keep_playing_animation := false
+var active_breakable_cell := Vector2i(-1, -1)
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var tilemap: TileMap = get_node("../TileMap")
@@ -65,32 +66,27 @@ func idle_animation() -> void:
 func _physics_process(delta: float) -> void:
 	move_to_target(delta)
 
-	if is_moving:
+	#Handling animations
+	if is_moving: #if already moving, keep moving
 		move_animation(move_direction)
-	else:
+	else: #Start
 		var direction := get_input_direction()
-
-		if direction != Vector2.ZERO:
+		if direction != Vector2.ZERO: 
 			start_move(direction)
-
-			if is_moving:
-				move_animation(direction)
-			else:
+			if is_moving == false: #if interfere with wall, idle
+				print("hi!")
 				idle_animation()
-		else:
+		else: #if zero, idle
 			idle_animation()
 
 func start_move(direction: Vector2) -> void:
 	if direction == Vector2.ZERO:
 		return
-
 	facing_direction = direction
-
 	if is_moving:
 		return
 	if test_move(global_transform, direction * TILE_SIZE):
-		return
-
+		return	
 	move_direction = direction
 	target_position = global_position + direction * TILE_SIZE
 	is_moving = true
@@ -101,6 +97,8 @@ func move_to_target(delta: float) -> void:
 	if remaining.length() <= step_distance:
 		global_position = target_position
 		if is_moving:
+			var current_cell := tilemap.local_to_map(tilemap.to_local(global_position))
+			break_active_tile(current_cell)
 			is_moving = false
 			apply_tile_effects()
 		return
@@ -117,12 +115,7 @@ func apply_tile_effects() -> void:
 	elif tile_type == "ice":
 		force_move(move_direction)
 	elif tile_type == "breakaway":
-		#func to count steps / animate it breaking
-		breakaway()
-		pass
-
-func breakaway() -> void:
-	print("Break!")
+		activate_breakable()
 
 func force_move(direction: Vector2) -> void:
 	if direction == Vector2.ZERO:
@@ -133,6 +126,19 @@ func force_move(direction: Vector2) -> void:
 	target_position = global_position + direction * TILE_SIZE
 	is_moving = true
 	move_animation(direction)
+
+func activate_breakable() -> void:
+	var cell := tilemap.local_to_map(tilemap.to_local(global_position))
+	active_breakable_cell = cell
+	tilemap.set_cell(0, cell, 4, Vector2i(9, 1), 0) #about to break tile
+	print("Activated breakaway at: ", cell)
+
+func break_active_tile(cell) -> void:
+	if active_breakable_cell == Vector2i(-1, -1):
+		return
+	tilemap.set_cell(0, active_breakable_cell, 4, Vector2i(10, 2), 0) # broken/hole tile
+	print("Broke tile at: ", active_breakable_cell)
+	active_breakable_cell = Vector2i(-1, -1)
 
 func get_current_tile_data() -> TileData:
 	var cell := tilemap.local_to_map(tilemap.to_local(global_position))
